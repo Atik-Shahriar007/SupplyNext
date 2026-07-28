30723d84f06766892e98ed27e0f31b508858d428
Atik-Shahriar007 <atikshahriar16@iut-dhaka.edu>
2026-07-29 02:51:44 +0600

    created the wartehouse repo and controller

---
+++ src/main/java/com/example/scmbackend/warehouse/WarehouseController.java
@@ -0,0 +1,26 @@
+package com.example.scmbackend.warehouse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/warehouses")
+public class WarehouseController {
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    // GET all warehouses
+    @GetMapping
+    public List<Warehouse> getAllWarehouses() {
+        return warehouseRepository.findAll();
+    }
+
+    // POST a new warehouse
+    @PostMapping
+    public Warehouse createWarehouse(@RequestBody Warehouse warehouse) {
+        return warehouseRepository.save(warehouse);
+    }
+}
