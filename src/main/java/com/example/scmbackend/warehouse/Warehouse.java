4f57a5d2d5dda304edc28d96526e72b60b73a9fd
Atik-Shahriar007 <atikshahriar16@iut-dhaka.edu>
2026-07-29 02:40:10 +0600

    Initial project setup

---
+++ src/main/java/com/example/scmbackend/warehouse/Warehouse.java
@@ -0,0 +1,26 @@
+package com.example.scmbackend.warehouse;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private Integer capacity;
+}
