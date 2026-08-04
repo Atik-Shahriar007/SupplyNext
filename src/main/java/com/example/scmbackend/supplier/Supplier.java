03a1d0da89e86076fd976afa0483ef2070e8b601
Atik-Shahriar007 <atikshahriar16@iut-dhaka.edu>
2026-08-04 23:18:33 +0600

    created the wartehouse repo and controller

---
+++ src/main/java/com/example/scmbackend/supplier/Supplier.java
@@ -0,0 +1,28 @@
+package com.example.scmbackend.supplier;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String contactPerson;
+    private String phone;
+    private String email;
+    private String address;
+}
